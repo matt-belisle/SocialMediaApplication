45e707baaa8638b06dbe880432f476c0f086cb8b
Matt <Matt Belisle>
2020-03-22 12:45:06 -0400

    SQL logging

--- src/main/kotlin/dataBase/DatabaseFactory.kt
+++ src/main/kotlin/dataBase/DatabaseFactory.kt
@@ -6,6 +6,8 @@ import dataBase.tables.AusPol2019DataTable
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.StdOutSqlLogger
+import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 
